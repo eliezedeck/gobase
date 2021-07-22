3bf14c605b3f1a6a10c018135cf35fbb884264c1
Elie Zedeck RANDRIAMIANDRIRAY <elie.zedeck@gmail.com>
2021-07-22 19:11:09 +0300

    Add 'date-iso8601' validator for ISO8601 dates

--- validation/validator.go
+++ validation/validator.go
@@ -1,10 +1,17 @@
 package validation
 
 import (
+	"regexp"
+
 	"github.com/go-playground/validator/v10"
 )
 
 var V = validator.New()
+
+var (
+	dateISO8601RegexStr = "^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])(?:T|\\s)(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])?(Z)?$"
+	dateISO8601Regex    = regexp.MustCompile(dateISO8601RegexStr)
+)
 
 // ValidatorErrorMessage returns the first error message after validation; if it's not an instance of ValidationErrors
 // then it will just return the normal .Error().
@@ -14,4 +21,12 @@ func ValidatorErrorMessage(err error) string {
 		return first.Error()
 	}
 	return err.Error()
+}
+
+func init() {
+	if err := V.RegisterValidation("date-iso8601", func(fl validator.FieldLevel) bool {
+		return dateISO8601Regex.MatchString(fl.Field().String())
+	}); err != nil {
+		panic(err)
+	}
 }
